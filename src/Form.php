7d208b8a9f4bf8ca2c926c65dae694d5e2dfe5a5
Sinevia <info@sinevia.com>
2018-05-26 21:32:02 +0100

    Update Form.php

--- src/Form.php
+++ src/Form.php
@@ -62,7 +62,7 @@ class Form extends Element {
         }
         $html = $indent . '<form' . $this->attributesToHtml() . $this->cssToHtml() . '>' . $nl;
         foreach ($this->children as $child) {
-            if (is_object($child) && is_subclass_of($child, "Sinevia\Ui\Element")) {
+            if (is_object($child) && is_subclass_of($child, "Sinevia\Html\Element")) {
                 $html .= $child->toHtml($compressed, $level + 1) . $nl;
             } else {
                 $html .= $indent . $tab . $child . $nl;
