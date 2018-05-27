e1d26403018e55f112d821f36cdf742c0679980f
Sinevia <info@sinevia.com>
2018-05-27 13:20:22 +0100

    Update Container.php

--- src/Container.php
+++ src/Container.php
@@ -1,28 +1,44 @@
 <?php
 
-    /**
-     * The Container class is an empty container, that displays its children.
-     * 
-     * It is suitable, if we want to add several widgets to a parent widget, without
-     * being wrapped in extra code. It will accept no style, attribute, etc.
-     * configurations. If you need to access the parent widget from children, use the
-     * S_Containers parent widget.
-     * <code>
-     * // Creating a new instance of Container
-     * $container = new S_Container();
-     *     $container->child(s_label()->text("One")->foreground("red"));
-     *     $container->child(s_label()->text("Two")->foreground("green"));
-     *     $container->child(s_label()->text("Three")->foreground("blue"));
-     *
-     * // Using the shortcut function
-     * $container = s_container()
-     *     ->child(s_label()->text("One")->foreground("red"));
-     *     ->child(s_label()->text("Two")->foreground("green"));
-     *     ->child(s_label()->text("Three")->foreground("blue"));
-     * </code>
-     * @package GUI
-     */
-class Container extends S_Widget{
+
+<?php
+// ========================================================================= //
+// SINEVIA PUBLIC                                        http://sinevia.com  //
+// ------------------------------------------------------------------------- //
+// COPYRIGHT (c) 2018 Sinevia Ltd                        All rights resrved! //
+// ------------------------------------------------------------------------- //
+// LICENCE: All information contained herein is, and remains, property of    //
+// Sinevia Ltd at all times.  Any intellectual and technical concepts        //
+// are proprietary to Sinevia Ltd and may be covered by existing patents,    //
+// patents in process, and are protected by trade secret or copyright law.   //
+// Dissemination or reproduction of this information is strictly forbidden   //
+// unless prior written permission is obtained from Sinevia Ltd per domain.  //
+//===========================================================================//
+namespace Sinevia\Html;
+
+/**
+* The Container class is an empty container, that displays its children.
+* 
+* It is suitable, if we want to add several widgets to a parent widget, without
+* being wrapped in extra code. It will accept no style, attribute, etc.
+* configurations. If you need to access the parent widget from children, use the
+* S_Containers parent widget.
+* <code>
+* // Creating a new instance of Container
+* $container = new S_Container();
+*     $container->child(s_label()->text("One")->foreground("red"));
+*     $container->child(s_label()->text("Two")->foreground("green"));
+*     $container->child(s_label()->text("Three")->foreground("blue"));
+*
+* // Using the shortcut function
+* $container = s_container()
+*     ->child(s_label()->text("One")->foreground("red"));
+*     ->child(s_label()->text("Two")->foreground("green"));
+*     ->child(s_label()->text("Three")->foreground("blue"));
+* </code>
+* @package GUI
+*/
+class Container extends Element{
     //========================= START OF METHOD ===========================//
     //  METHOD: to_html                                                    //
     //=====================================================================//
