f9bf189798f856574b26999431bc68af9ffb748b
Sinevia <info@sinevia.com>
2018-05-29 08:55:13 +0100

    Update Select.php

--- src/Select.php
+++ src/Select.php
@@ -1,5 +1,19 @@
 <?php
 
+// ========================================================================= //
+// SINEVIA PUBLIC                                        http://sinevia.com  //
+// ------------------------------------------------------------------------- //
+// COPYRIGHT (c) 2018 Sinevia Ltd                        All rights resrved! //
+// ------------------------------------------------------------------------- //
+// LICENCE: All information contained herein is, and remains, property of    //
+// Sinevia Ltd at all times.  Any intellectual and technical concepts        //
+// are proprietary to Sinevia Ltd and may be covered by existing patents,    //
+// patents in process, and are protected by trade secret or copyright law.   //
+// Dissemination or reproduction of this information is strictly forbidden   //
+// unless prior written permission is obtained from Sinevia Ltd per domain.  //
+//===========================================================================//
+namespace Sinevia\Html;
+
     /**
      * The Select creates an HTML control widget for selection of options.
      * It is most often used in Forms.
