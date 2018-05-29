09ebf727e2163694ae863888ee5809febed3b433
Sinevia <info@sinevia.com>
2018-05-29 08:55:51 +0100

    Update Font.php

--- src/Font.php
+++ src/Font.php
@@ -1,5 +1,20 @@
 <?php
 
+// ========================================================================= //
+// SINEVIA PUBLIC                                        http://sinevia.com  //
+// ------------------------------------------------------------------------- //
+// COPYRIGHT (c) 2018 Sinevia Ltd                        All rights resrved! //
+// ------------------------------------------------------------------------- //
+// LICENCE: All information contained herein is, and remains, property of    //
+// Sinevia Ltd at all times.  Any intellectual and technical concepts        //
+// are proprietary to Sinevia Ltd and may be covered by existing patents,    //
+// patents in process, and are protected by trade secret or copyright law.   //
+// Dissemination or reproduction of this information is strictly forbidden   //
+// unless prior written permission is obtained from Sinevia Ltd per domain.  //
+//===========================================================================//
+
+namespace Sinevia\Html;
+
     /**
      * The Font class makes working with fonts easy and intuitive.
      * The Font class supports method chaining, which makes it suitable
