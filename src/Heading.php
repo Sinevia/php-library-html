be28a689f426e944894e9fed167acb7a35cbd19d
Sinevia <info@sinevia.com>
2018-05-30 03:15:18 +0100

    Update Heading.php

--- src/Heading.php
+++ src/Heading.php
@@ -1,4 +1,5 @@
 <?php
+
 // ========================================================================= //
 // SINEVIA PUBLIC                                        http://sinevia.com  //
 // ------------------------------------------------------------------------- //
@@ -25,10 +26,42 @@ class Heading extends Element {
         parent::__construct();
     }
     
+    /**
+     * Returns the text of this Heading.
+     * @return String The text as String (null, if not set)
+     */
+    public function getText() {
+        return $this->getProperty('text');
+    }
+
+    /**
+     * Sets he text of this Heading.
+     * @param String the text to be shown
+     * @return Heading the instance of this Heading
+     * @throws \InvalidArgumentException if parameter $text is not is not String
+     */
+    public function setText($text = null) {
+        if (is_string($text) == false) {
+            throw new \InvalidArgumentException('In class <b>' . get_class($this) . '</b> in method <b>text($text)</b>: Parameter <b>$text</b> MUST BE of type String - <b>' . (is_object($text) ? get_class($text) : gettype($text)) . '</b> given!');
+        }
+        $this->setProperty('text', $text);
+        return $this;
+    }
+    
+    /**
+     * Returns the weight of this Heading.
+     * @return int The weight as integer (1, if not set)
+     */
     function getWeight() {
         return $this->weight;
     }
 
+    /**
+     * Sets the weight of this Heading (an integer of 1 to 6).
+     * @param int the weight to be shown
+     * @return Heading the instance of this Heading
+     * @throws \InvalidArgumentException if parameter $text is not is not an integer
+     */
     function setWeight($weight) {
         if (is_int($weight)) {
             throw new \InvalidArgumentException('In class ' . get_class($this) . ' in method setWeight($weight): Parameter $weight MUST BE of type Integer - ' . (is_object($weight) ? get_class($weight) : gettype($weight)) . ' given!');
@@ -54,10 +87,13 @@ class Heading extends Element {
             $tab = "";
             $indent = "";
         }
-        if ($this->getProperty('text') == null) {
-            $this->getProperty('text', "&nbsp;");
-        }
+        
         $html = $indent . '<h' . $this->weight . $this->attributesToHtml() . $this->cssToHtml() . '>' . $nl;
+        
+        if ($this->getProperty("text") != null) {
+            $html .= $indent . $tab . $this->getProperty("text") . $nl;
+        }
+        
         foreach ($this->children as $child) {
             if (is_object($child) && is_subclass_of($child, "Sinevia\Html\Element")) {
                 $html .= $child->toHtml($compressed, $level + 1) . $nl;
